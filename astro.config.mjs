289385ecaabfc6539cf29a3bb913506e3bfcd8aa
Akash NP <akashnp1998@gmail.com>
2022-11-06 14:19:50 +0530

    Add ghpage deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://9akashnp8.github.io',
+    base: '/tested-transit'
+});
